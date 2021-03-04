d92ca20e30fbe6a5c150e1c478e61020402dd2ae
Newp <mynewp@gmail.com>
2021-03-05 01:16:10 +0900

    타입 T를 받아서 모든 맴버들에 대해 operator를 연산해주는 기능 추가

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using Npgg.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class Stat
+        {
+            public int Attack { get; set; }
+            public int MaxHP { get; set; }
+
+            public static Stat operator +(Stat a, Stat b)
+            {
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Generate<Stat>();
+        }
+
+        static void Generate<T>()
+        {
+            var stats = MemberAccessor.GetAccessors<T>();
+            var name = typeof(T).Name;
+            var sb = new StringBuilder();
+
+            foreach (var op in new[] { "+", "-"})
+            {
+                sb.Append($"public static {name} operator {op}({name} a, {name} b) =>\n");
+                sb.Append($"new {name}() {{\n");
+                foreach (var member in stats)
+                {
+                    var fieldName = member.Key;
+                    sb.Append('\t');
+                    sb.Append($"{fieldName} = a.{fieldName} {op} b.{fieldName},");
+                    sb.Append('\n');
+                }
+                sb.Append("};\n\n");
+            }
+        }
+
+    }
+}
