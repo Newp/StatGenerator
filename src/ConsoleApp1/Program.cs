07ebdacc7b4c218cb689bbe6e0dda0b18a777a90
Newp <mynewp@gmail.com>
2021-03-07 22:33:30 +0900

    리플랙션쓰지 않고 name , keyValue로 바꿈! 훨씬깔끔

--- src/ConsoleApp1/Program.cs
+++ src/ConsoleApp1/Program.cs
@@ -1,46 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
-using Npgg.Reflection;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public class Stat
-        {
-            public int Attack { get; set; }
-            public int MaxHP { get; set; }
-
-            public static Stat operator +(Stat a, Stat b)
-            {
-                return null;
-            }
-        }
 
         static void Main(string[] args)
         {
-            Generate<Stat>();
+            Dictionary<string, string> types = new Dictionary<string, string>()
+            {
+                { "Strength", "int" },
+                { "Intelligence","int" },
+                { "Vitality","int" },
+            };
+
+            Generate("Stat", types);
         }
 
-        static void Generate<T>()
+        static void Generate(string name, Dictionary<string, string> dictionary)
         {
-            var stats = MemberAccessor.GetAccessors<T>();
-            var name = typeof(T).Name;
             var sb = new StringBuilder();
 
-            foreach (var op in new[] { "+", "-"})
+            sb.AppendLine($"public partial class {name}");
+            sb.AppendLine("{");
+            foreach (var member in dictionary)
             {
-                sb.Append($"public static {name} operator {op}({name} a, {name} b) =>\n");
-                sb.Append($"new {name}() {{\n");
-                foreach (var member in stats)
+                sb.AppendLine($"\tpublic {member.Value} {member.Key} {{ get; set; }}");
+            }
+
+            foreach (var op in new[] { "+", "-" })
+            {
+                sb.Append($"\tpublic static {name} operator {op}({name} a, {name} b) =>\n");
+                sb.Append($"\t\tnew {name}() {{\n");
+                foreach (var member in dictionary)
                 {
                     var fieldName = member.Key;
-                    sb.Append('\t');
+                    sb.Append("\t\t\t");
                     sb.Append($"{fieldName} = a.{fieldName} {op} b.{fieldName},");
                     sb.Append('\n');
                 }
-                sb.Append("};\n\n");
+                sb.Append("\t\t};\n\n");
             }
+
+            sb.AppendLine("}");
         }
 
     }
